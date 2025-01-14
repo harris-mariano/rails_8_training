cbadd3fb23c5b2538be5a092e3bde5f284c7e80c
Harris Mariano <harris.mariano@adish.com.ph>
2025-01-15 02:07:32 +0800

    Initial Commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,28 @@
+class CommentsController < ApplicationController
+  before_action :set_article, only: [ :create, :destroy ]
+  before_action :set_comment, only: [ :destroy ]
+
+  def create
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to article_path(@article), status: :see_other
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:article_id])
+  end
+
+  def set_comment
+    @comment = @article.comments.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
+  end
+end
